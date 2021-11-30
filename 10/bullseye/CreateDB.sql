027cf7157c354c217eae7f5e4350f8f67241ca41
velito86 <67463332+velito86@users.noreply.github.com>
2021-11-30 10:16:33 +0200

    Update CreateDB.sql

--- 10/bullseye/CreateDB.sql
+++ 10/bullseye/CreateDB.sql
@@ -1,1 +1,1 @@
-create table entries (id serial primary key, key varchar(255) not null, value text not null, expiration int, unique(key));
+create table entries (id serial primary key, key varchar(255) not null, value text not null, expiration bigint, unique(key));
